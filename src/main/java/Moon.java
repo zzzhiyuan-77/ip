69921f9f6618d5c8772dd32642e74b3d22b43843
zzzhiyuan-77 <nizhiyuan125@gmail.com>
2026-08-23 17:21:57 +0800

    Level 1

--- src/main/java/Moon.java
+++ src/main/java/Moon.java
@@ -1,12 +1,32 @@
+import java.util.Scanner;
+
 public class Moon {
+    /**
+     * Starts the chatbot and reads commands from the user.
+     *
+     * @param args command-line arguments, which are not used by this program
+     */
     public static void main(String[] args) {
-        String text = "____________________________________________________________\n"
-                        + "Hello! I'm Moon, your personal chatbot.\n"
-                        + "What can I do for you?\n"
-                        + "____________________________________________________________\n"
-                        + "Bye. Hope to see you again soon!\n"
-                        + "____________________________________________________________"
-        ;
-        System.out.println(text);
+        String divider = "____________________________________________________________";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(divider);
+        System.out.println("Hello! I'm Moon, your personal chatbot.");
+        System.out.println("What can I do for you?");
+        System.out.println(divider);
+
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+
+            System.out.println(divider);
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(divider);
+                return;
+            }
+
+            System.out.println(" " + command);
+            System.out.println(divider);
+        }
     }
 }
