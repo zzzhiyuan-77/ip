563de993ef1f0861755ec2d60c1d1e11b8a66a5c
zzzhiyuan-77 <nizhiyuan125@gmail.com>
2026-08-23 18:09:52 +0800

    Unmark command added

--- src/main/java/Moon.java
+++ src/main/java/Moon.java
@@ -40,6 +40,12 @@ public class Moon {
                 isDone[taskIndex] = true;
                 System.out.println(" Nice! I've marked this task as done:");
                 System.out.println("   [X] " + tasks[taskIndex]);
+            } else if (command.startsWith("unmark ")) {
+                int taskNumber = Integer.parseInt(command.substring(7));
+                int taskIndex = taskNumber - 1;
+                isDone[taskIndex] = false;
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println("   [ ] " + tasks[taskIndex]);
             } else {
                 tasks[taskCount] = command;
                 taskCount++;
